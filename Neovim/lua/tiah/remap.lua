cb791ad52ec954ff9fc9cd79877815b8808ffd3d
Antonio Carrillo <antonio.cruz.carrillo@gmail.com>
2024-07-29 17:29:26 -0600

    * and # centered

--- Neovim/lua/tiah/remap.lua
+++ Neovim/lua/tiah/remap.lua
@@ -14,6 +14,8 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('n', '*', '*zzzv')
+vim.keymap.set('n', '#', '#zzzv')
 
 -- Copy and pasting
 vim.keymap.set('x', '<leader>p', '"_dp')

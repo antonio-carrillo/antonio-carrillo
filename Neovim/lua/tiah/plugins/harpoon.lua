c2a7fc2fea1d84f1d9c3d4d13350966aec7989e4
Antonio Carrillo <antonio.cruz.carrillo@gmail.com>
2024-07-29 17:29:06 -0600

    Harpoon with 9 files

--- Neovim/lua/tiah/plugins/harpoon.lua
+++ Neovim/lua/tiah/plugins/harpoon.lua
@@ -7,9 +7,8 @@ return {
     vim.keymap.set('n', '<leader>a', mark.add_file)
     vim.keymap.set('n', '<leader>e', ui.toggle_quick_menu)
 
-    vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end)
-    vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end)
-    vim.keymap.set('n', '<leader>3', function() ui.nav_file(3) end)
-    vim.keymap.set('n', '<leader>4', function() ui.nav_file(4) end)
+    for i = 1, 9 do
+      vim.keymap.set('n', '<leader>' .. i, function() ui.nav_file(i) end)
+    end
   end,
 }

7e29274363278d62e04e716825ca2039ffb5cab4
Antonio Carrillo <dev.antonio.carrillo@gmail.com>
2024-08-03 20:21:22 -0600

    aliases for WSL own file

--- Zsh/aliases.zsh
+++ Zsh/aliases.zsh
@@ -1,8 +1,4 @@
 # Quick scripts
 alias pip-essentials='pip install wheel pynvim jedi autopep8 flake8 bpython'
+alias v=nvim
 alias vera='veracrypt -m=nokernelcrypto'
-
-# Software
-alias drawio='$PROGRAMS/draw.io/draw.io.exe'
-alias firefox="$WINDOWS/Program\ Files/Mozilla\ Firefox/firefox.exe"
-alias start='powershell.exe start'
---
+++ Zsh/aliases_wsl.zsh
@@ -0,0 +1,4 @@
+# Software
+alias drawio='$PROGRAMS/draw.io/draw.io.exe'
+alias firefox="$WINDOWS/Program\\ Files/Mozilla\\ Firefox/firefox.exe"
+alias start='powershell.exe start'
